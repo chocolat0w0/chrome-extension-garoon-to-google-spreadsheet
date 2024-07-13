5618065c8c2b90669e3bc303d43a4119695a887a
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2024-07-13 11:28:40 +0900

    GaroonのURLを設定できる

--- options/options.js
+++ options/options.js
@@ -1,18 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  chrome.storage.sync.get(["spreadsheetId", "sheetName"], (result) => {
-    if (result.spreadsheetId) {
-      document.getElementById("spreadsheetId").value = result.spreadsheetId;
+  chrome.storage.sync.get(
+    ["garoonDomain", "spreadsheetId", "sheetName"],
+    (result) => {
+      if (result.garoonDomain) {
+        document.getElementById("spreadsheetId").value = result.garoonDomain;
+      }
+      if (result.spreadsheetId) {
+        document.getElementById("spreadsheetId").value = result.spreadsheetId;
+      }
+      if (result.sheetName) {
+        document.getElementById("sheetName").value = result.sheetName;
+      }
     }
-    if (result.sheetName) {
-      document.getElementById("sheetName").value = result.sheetName;
-    }
-  });
+  );
 
   document.getElementById("save").addEventListener("click", () => {
+    const garoonDomain = document.getElementById("garoonDomain").value;
     const spreadsheetId = document.getElementById("spreadsheetId").value;
     const sheetName = document.getElementById("sheetName").value;
-    chrome.storage.sync.set({ spreadsheetId, sheetName }, () => {
-      console.log("Spreadsheet ID is saved:", spreadsheetId);
+    chrome.storage.sync.set({ garoonDomain, spreadsheetId, sheetName }, () => {
+      console.log("Saved", garoonDomain, spreadsheetId, sheetName);
     });
   });
 });
