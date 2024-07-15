f7e842c2e72be9963513fa1f59a602b887de08d8
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2024-07-15 11:57:02 +0900

    Style options page for improved appearance
    Closes #2

--- options/options.js
+++ options/options.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const savedElem = document.getElementById("saved");
   chrome.storage.sync.get(
     ["garoonDomain", "spreadsheetId", "sheetName"],
     (result) => {
@@ -14,12 +15,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   );
 
+  [...document.getElementsByTagName("input")].map((target) => {
+    target.addEventListener("input", () => {
+      savedElem.style.display = "none";
+    });
+  });
+
   document.getElementById("save").addEventListener("click", () => {
     const garoonDomain = document.getElementById("garoonDomain").value;
     const spreadsheetId = document.getElementById("spreadsheetId").value;
     const sheetName = document.getElementById("sheetName").value;
     chrome.storage.sync.set({ garoonDomain, spreadsheetId, sheetName }, () => {
-      console.log("Saved", garoonDomain, spreadsheetId, sheetName);
+      savedElem.style.display = "inline";
     });
   });
 });
