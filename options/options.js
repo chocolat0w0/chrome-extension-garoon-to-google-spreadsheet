c3108f10d5b336288a8f2ccbee3bb214811eb7dd
chocolat0w0 <2049423+chocolat0w0@users.noreply.github.com>
2024-07-15 12:50:05 +0900

    Allow setting data retrieval interval in options

--- options/options.js
+++ options/options.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   const savedElem = document.getElementById("saved");
+  const errorElem = document.getElementById("error");
+
   chrome.storage.sync.get(
-    ["garoonDomain", "spreadsheetId", "sheetName"],
+    ["garoonDomain", "garoonInterval", "spreadsheetId", "sheetName"],
     (result) => {
       if (result.garoonDomain) {
         document.getElementById("garoonDomain").value = result.garoonDomain;
+      }
+      if (result.garoonInterval) {
+        document.getElementById("garoonInterval").value = result.garoonInterval;
       }
       if (result.spreadsheetId) {
         document.getElementById("spreadsheetId").value = result.spreadsheetId;
@@ -18,15 +23,34 @@ document.addEventListener("DOMContentLoaded", () => {
   [...document.getElementsByTagName("input")].map((target) => {
     target.addEventListener("input", () => {
       savedElem.style.display = "none";
+      errorElem.style.display = "none";
     });
   });
 
   document.getElementById("save").addEventListener("click", () => {
     const garoonDomain = document.getElementById("garoonDomain").value;
+    const garoonInterval = Number(
+      document.getElementById("garoonInterval").value
+    );
     const spreadsheetId = document.getElementById("spreadsheetId").value;
     const sheetName = document.getElementById("sheetName").value;
-    chrome.storage.sync.set({ garoonDomain, spreadsheetId, sheetName }, () => {
-      savedElem.style.display = "inline";
+
+    if (Number.isNaN(garoonInterval)) {
+      errorElem.style.display = "inline";
+      return;
+    }
+
+    chrome.storage.sync.set(
+      { garoonDomain, garoonInterval, spreadsheetId, sheetName },
+      () => {
+        savedElem.style.display = "inline";
+      }
+    );
+
+    chrome.alarms.clear("garoonAlarm", (wasCleared) => {
+      chrome.alarms.create("garoonAlarm", {
+        periodInMinutes: garoonInterval,
+      });
     });
   });
 });
